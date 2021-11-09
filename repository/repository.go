90bd99947484c9ed090b0574796473cf32859b8a
logica <84759675+logica0419@users.noreply.github.com>
2021-11-10 03:25:06 +0900

    deleted inefficient declearation in repository

--- repository/repository.go
+++ repository/repository.go
@@ -1,15 +1,7 @@
 package repository
 
-import "gorm.io/gorm"
-
 // DB 操作リポジトリ
 type Repository interface {
-	// マイグレーションを実行
-	migrate() error
-
-	// 新たなトランザクション用 DB セッションを取得
-	getTx() *gorm.DB
-
 	// 予約投稿テーブル操作リポジトリ
 	SchMesRepository
 
