4962b682b36941345c690a4c1bfa7ecf39bfb28c
logica <84759675+logica0419@users.noreply.github.com>
2021-11-14 00:18:28 +0900

    updated comment

--- service/parser/parser_schedule.go
+++ service/parser/parser_schedule.go
@@ -18,7 +18,7 @@ func argparseScheduleCommand(command []string) (*string, *string, *string, *int,
 	postTime := parser.String("t", "time", &argparse.Options{Required: true,
 		Help: "メッセージを送る時間 予約投稿フォーマット:`yyyy/mm/dd/hh:mm` 定期投稿フォーマット:`yyyy/mm/dd/hh:mm/d(曜日)` 曜日(オプション)は0が日曜、6が土曜に対応する1桁の整数で記入して下さい 曜日を&で繋げると複数曜日の同じ時間を同時指定できます"})
 	body := parser.String("b", "body", &argparse.Options{Required: true,
-		Help: "送るメッセージ スペースが入るときは\"\"や''でくくって下さい 予約メッセージではメンションせずにbodyにメンションを入れたい場合、\"@.{id}\"と打てば自動で\"@{id}\"に変換されます 改行したい場合は\"\"や''でくくった上で、改行したい箇所に\\nを挿入するか 通常通り改行して下さい"})
+		Help: "送るメッセージ スペースが入るときは\"\"や''でくくって下さい 予約メッセージではメンションせずにbodyにメンションを入れたい場合、 \"@.{id}\"と打てば自動で\"@{id}\"に変換されます 改行したい場合は\"\"や''でくくった上で、改行したい箇所に\\nを挿入するか 通常通り改行して下さい"})
 	repeat := parser.Int("r", "repeat", &argparse.Options{Help: "(定期投稿のみ) 投稿を繰り返す回数 1以上の回数を指定してください 0か省略で無限に繰り返します"})
 	parser.DisableHelp()
 
